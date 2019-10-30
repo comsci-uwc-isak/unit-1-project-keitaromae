38e7bace4fd8878c5c22eb3e7a919adc8af751ba
keitaromae <54606445+keitaromae@users.noreply.github.com>
2019-10-30 09:17:59 +0900

    Add files via upload

---
+++ editcar.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#This program edit the information of an exiting car in the
+#maincarfile
+#user enters [license place] [model] [red] [pp]
+
+if [ $# -ne 4 ]; then
+  echo "Error with the number of arguments"
+  echo "Enter License Maker Model Passengers"
+  exit
+fi
+
+license=$1
+maker=$2
+model=$3
+pp=$4
+
+cd ../Database
+
+if [ ! -f "$license.txt" ]; then
+  echo "File not found!"
+fi
+
+#find the line with the given car plate and delete it
+sed -i '' "/^$license/d" maincarfile.txt
+#add the new information
+echo "$license $maker $model $pp" >> maincarfile.txt
+cd ../scripts
+bash frame2 "Car edited successfully"
+
